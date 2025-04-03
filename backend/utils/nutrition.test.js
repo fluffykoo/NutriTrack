3e6ca310a020c2bab5de3748e89c00d94925cdf3
fluffykoo <oumou.camara@efrei.net>
2025-04-03 19:02:51 +0200

    Ajout graphique historique, moyenne journalière, test Jest, nettoyage du code, améliorations UI (feedback, couleurs, masquage connexion)

--- backend/utils/nutrition.test.js
+++ backend/utils/nutrition.test.js
@@ -1,19 +1,19 @@
 const { getDailyTotals } = require('./nutrition');
 
 describe('getDailyTotals', () => {
-  it('calcule les totaux correctement', () => {
+it('calcule les totaux correctement', () => {
     const repas = [
-      { calories: 500, proteines: 30, glucides: 50, lipides: 20 },
-      { calories: 400, proteines: 20, glucides: 40, lipides: 10 }
+    { calories: 500, proteines: 30, glucides: 50, lipides: 20 },
+    { calories: 400, proteines: 20, glucides: 40, lipides: 10 }
     ];
 
     const resultat = getDailyTotals(repas);
 
     expect(resultat).toEqual({
-      calories: 900,
-      proteines: 50,
-      glucides: 90,
-      lipides: 30
+    calories: 900,
+    proteines: 50,
+    glucides: 90,
+    lipides: 30
     });
-  });
+});
 });
